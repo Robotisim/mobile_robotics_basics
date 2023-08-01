fd612d1a2ba496c3f1dff23a98beef3ef2f0af4b
Ibrahim Mansur <ibrahimmansur4@gmail.com>
2023-08-01 15:39:46 +0500

    Udp communication #6

---
+++ src/udp_send.py
@@ -0,0 +1,10 @@
+import socket
+
+UDP_IP = "192.168.100.11"
+UDP_PORT = 8888
+MESSAGE = "0,0,0,0,1"
+
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(bytes(MESSAGE.encode('utf-8')), (UDP_IP, UDP_PORT))
