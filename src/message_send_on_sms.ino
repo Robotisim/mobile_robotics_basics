2753ac44d5676e9bc52d068c40540e61f47bb8ac
khubaibiftikhar <khubaibiftikhar07@gmail.com>
2023-08-05 12:26:48 +0500

    APP to driving and OLED display #8

--- src/message_send_on_sms.ino
+++ src/message_send_on_sms.ino
@@ -14,7 +14,7 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.println("Connecting to WiFi....");
   }
 
   Serial.println("Connected to WiFi");
