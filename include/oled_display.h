31854ccb0041acdcb63635880e3ea79a45b60744
noshluk2 <noshluk2@gmail.com>
2023-08-29 10:01:29 +0500

    Starting Recording

--- include/oled_display.h
+++
@@ -1,7 +0,0 @@
-#ifndef OLED_DISPLAY_H
-#define OLED_DISPLAY_H
-
-void setupOLED();
-void displayMultipleFloats(const char *label1, float value1, const char *label2, float value2);
-
-#endif // OLED_DISPLAY_H
