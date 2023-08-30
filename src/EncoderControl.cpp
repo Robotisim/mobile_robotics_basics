12928f873f983c7a2701a2b86fd9c4d4bf7447c4
Ibrahim <ibrahimmansur4@gmail.com>
2023-08-30 12:17:25 +0500

    kinematics eq updated #17

--- src/EncoderControl.cpp
+++ src/EncoderControl.cpp
@@ -7,6 +7,14 @@
 AS5600 as5600_0(&Wire); // use default Wire
 AS5600 as5600_1(&Wire1);
 
+
+/*
+The AS5600 allows controlling the direction of the magnet rotation with the DIR pin. 
+If DIR is connected to GND (DIR = 0) a clockwise rotation viewed from the top will 
+generate an increment of the calculated angle. If the DIR pin is connected 
+to VDD (DIR = 1) an increment of the calculated angle will happen with 
+counter-clockwise rotation.
+*/
 void setupEncoder0()
 {
   Wire.begin();
@@ -17,8 +25,9 @@ void setupEncoder0()
   }
   else
   {
-    as5600_0.setDirection(AS5600_CLOCK_WISE);
+    as5600_0.setDirection(AS5600_CLOCK_WISE); 
     Serial.println("Connect device 0: true");
+    as5600_0.resetCumulativePosition(0);
   }
 }
 
@@ -34,8 +43,10 @@ void setupEncoder1()
   {
     as5600_1.setDirection(AS5600_CLOCK_WISE);
     Serial.println("Connect device 1: true");
+    as5600_1.resetCumulativePosition(0);
   }
 }
+
 int cummulativePos0()
 {
   if (!as5600_0.begin())
@@ -44,10 +55,10 @@ int cummulativePos0()
   else
   {
     //  set initial position
-    as5600_0.resetPosition();
-    // as5600_0.getCumulativePosition();
+
+    int cummulative0 = as5600_0.getCumulativePosition();
     //  should be enough up to ~200 RPM
-    int cummulative0 = as5600_0.readAngle();  //right
+    // int cummulative0 = as5600_0.readAngle();  //right
     Serial.println(cummulative0);
     return cummulative0;
   }
@@ -61,10 +72,10 @@ int cummulativePos1()
   else
   {
     //  set initial position
-    as5600_1.resetPosition();
-    // as5600_1.getCumulativePosition();
+    
+    int cummulative1 = as5600_1.getCumulativePosition();
     //  should be enough up to ~200 RPM
-    int cummulative1 = as5600_1.readAngle();
+    // int cummulative1 = as5600_1.readAngle();
     Serial.println(cummulative1);
     return cummulative1;
   }
--- src/dead_Reckoning.cpp
+++ src/dead_Reckoning.cpp
@@ -3,54 +3,68 @@
 #include <Arduino.h>
 #include "dead_Reckoning.h"
 #include "EncoderControl.h"
-//Fixed values
+
+// Fixed values
 #define WHEEL_DIAMETER 0.0335
 #define PULSES_PER_REVOLUTION 4096.0
-#define AXLE_LENGTH  0.067
-#define PI 3.1416
+#define AXLE_LENGTH 0.067
+#define PI 3.1415926535897932384626433832795
 
 // New Variables
-float disp_r_wheel       = 0;
-float disp_l_wheel       = 0;
-int count_R_prev         = 0;
-int count_L_prev         = 0;             
-float x                  = 0;
-float y                  = 0;
-float theta              = 0;
-float meter_per_ticks    = PI * WHEEL_DIAMETER / PULSES_PER_REVOLUTION;
-float orientation_angle , disp_body;
+float disp_r_wheel = 0;
+float disp_l_wheel = 0;
+int count_R_prev = 0;
+int count_L_prev = 0;
+float x = 0;
+float y = 0;
+float theta = 0;
+float meter_per_ticks = PI * WHEEL_DIAMETER / PULSES_PER_REVOLUTION;
+float orientation_angle, disp_body;
 
 // Old Variables
-int count_R              = 0;                          //For Encoders
-int count_L              = 0;  
-          
-void calculate_traveling(){
-      
-    count_L_prev = count_L;
-    count_R_prev = count_R;
-    count_L      = cummulativePos0();
-    count_R      = cummulativePos1();
-    disp_l_wheel = (float)count_L_prev * meter_per_ticks;              // geting distance in meters each wheel has traveled
-    disp_r_wheel = (float)count_R_prev * meter_per_ticks;
-    
-    if (count_L_prev == count_R_prev)
-    {                                                                  // The Straight line condition -> book reference Where am i ?
-      x += disp_l_wheel * cos(theta);
-      y += disp_l_wheel * sin(theta);
-    }
-    else                                                               // for circular arc equations change
-    { orientation_angle = (disp_r_wheel - disp_l_wheel)/AXLE_LENGTH;
-      disp_body   = (disp_r_wheel + disp_l_wheel) / 2.0;
-      x += (disp_body/orientation_angle) * (sin(orientation_angle + theta) - sin(theta));
-      y -= (disp_body/orientation_angle) * (cos(orientation_angle + theta) - cos(theta));
-      theta += orientation_angle;
+int count_R = 0; // For Encoders
+int count_L = 0;
 
-     
-       while(theta > PI)
-         theta -= (2.0*PI);
-       while(theta < -PI) 
-         theta += (2.0*PI); 
-    }
-} 
-  
+void calculate_traveling()
+{
+  int cumulativePos0 = cummulativePos0(); // Get encoder values
+  int cumulativePos1 = cummulativePos1();
 
+  int deltaPos0 = cumulativePos0 - count_R_prev;
+  int deltaPos1 = cumulativePos1 - count_L_prev;
+
+  count_L_prev = cumulativePos1;
+  count_R_prev = cumulativePos0;
+
+  float disp_l_wheel = deltaPos1 * meter_per_ticks;
+  float disp_r_wheel = deltaPos0 * meter_per_ticks;
+  Serial.print("disp_l_wheel : ");
+  Serial.print(disp_l_wheel);
+  Serial.print(" | disp_r_wheel : ");
+  Serial.println(disp_r_wheel);
+  if (deltaPos0 == deltaPos1)
+  { // The Straight line condition -> book reference Where am i ?
+    x += disp_l_wheel * cos(theta);
+    y += disp_l_wheel * sin(theta);
+  }
+  else // for circular arc equations change
+  {
+    orientation_angle = (disp_r_wheel - disp_l_wheel) / AXLE_LENGTH;
+    disp_body = (disp_r_wheel + disp_l_wheel) / 2.0;
+    x += (disp_body / orientation_angle) * (sin(orientation_angle + theta) - sin(theta));
+    y -= (disp_body / orientation_angle) * (cos(orientation_angle + theta) - cos(theta));
+    theta += orientation_angle;
+    Serial.print("orientation_angle : ");
+    Serial.print(orientation_angle);
+    Serial.print(" | theta : ");
+    Serial.println(theta);
+    while (theta > PI)
+      theta -= (2.0 * PI);
+    while (theta < -PI)
+      theta += (2.0 * PI);
+  }
+  Serial.print("count_L : ");
+  Serial.print(count_L_prev);
+  Serial.print(" | count_R : ");
+  Serial.println(count_R_prev);
+}
